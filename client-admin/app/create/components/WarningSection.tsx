1deb21fd619bea969d2b2d0b0ade3cd54053ede3
Kitaro Yamaoka <takeroku2008@gmail.com>
2025-06-11 23:31:28 +0900

    追加の対応

--- client-admin/app/create/components/WarningSection.tsx
+++ client-admin/app/create/components/WarningSection.tsx
@@ -8,11 +8,9 @@ export function WarningSection() {
     <Stack gap="4" width="full">
       <Alert.Root status="warning">
         <Alert.Indicator />
-        <Alert.Title>レポートの作成には数分から数十分程度の時間がかかります</Alert.Title>
-      </Alert.Root>
-      <Alert.Root status="warning">
-        <Alert.Indicator />
-        <Alert.Title>作成が完了したレポートが一覧画面に反映されるまで5分程度かかります</Alert.Title>
+        <Alert.Title>
+          レポートの作成には数分かかる可能性があります。試験環境のため５分を超える処理は失敗する可能性があります。失敗する場合は処理のスキップや、データ量の調整などを試みてください
+        </Alert.Title>
       </Alert.Root>
     </Stack>
   );
